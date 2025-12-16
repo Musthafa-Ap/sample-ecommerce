863848ffa7a3cbad2a2698544df8faec1aeb94f2
Musthafa <musthafamohammed398@gmail.com>
2025-12-16 21:33:04 +0530

    favorite icon functionality implemented

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -1,10 +1,12 @@
 class ProductModel {
+  final int id;
   final String name;
   final String image;
   final String description;
   final double price;
 
   ProductModel({
+    required this.id,
     required this.name,
     required this.image,
     required this.description,
--- lib/products_listing_screen.dart
+++ lib/products_listing_screen.dart
@@ -5,13 +5,21 @@ import 'package:sample_app/product_model.dart';
 
 import 'common_app_bar.dart';
 
-class ProductsListingScreen extends StatelessWidget {
+class ProductsListingScreen extends StatefulWidget {
   const ProductsListingScreen({super.key});
+
+  @override
+  State<ProductsListingScreen> createState() => _ProductsListingScreenState();
+}
+
+class _ProductsListingScreenState extends State<ProductsListingScreen> {
+  List wishlistItems = [];
 
   @override
   Widget build(BuildContext context) {
     List<ProductModel> productsList = [
       ProductModel(
+        id: 1,
         name: '21WN',
         image: 'assets/images/product1.png',
         description: 'reversible angora cardigan',
@@ -19,48 +27,56 @@ class ProductsListingScreen extends StatelessWidget {
       ),
 
       ProductModel(
+        id: 2,
         name: '21SS',
         image: 'assets/images/product2.png',
         description: 'cotton casual shirt',
         price: 80,
       ),
       ProductModel(
+        id: 3,
         name: '21FW',
         image: 'assets/images/product3.png',
         description: 'leather boots',
         price: 150,
       ),
       ProductModel(
+        id: 4,
         name: '22SP',
         image: 'assets/images/product4.png',
         description: 'denim jeans',
         price: 90,
       ),
       ProductModel(
+        id: 5,
         name: '22SS',
         image: 'assets/images/product1.png',
         description: 'cotton casual shirt',
         price: 80,
       ),
       ProductModel(
+        id: 6,
         name: '22FW',
         image: 'assets/images/product2.png',
         description: 'leather boots',
         price: 150,
       ),
       ProductModel(
+        id: 7,
         name: '23SP',
         image: 'assets/images/product3.png',
         description: 'denim jeans',
         price: 90,
       ),
       ProductModel(
+        id: 8,
         name: '23SS',
         image: 'assets/images/product4.png',
         description: 'cotton casual shirt',
         price: 80,
       ),
       ProductModel(
+        id: 9,
         name: '23FW',
         image: 'assets/images/product1.png',
         description: 'leather boots',
@@ -123,9 +139,16 @@ class ProductsListingScreen extends StatelessWidget {
                           ),
                           alignment: Alignment.bottomRight,
                           child: IconButton(
-                            onPressed: () {},
+                            onPressed: () {
+                             setState(() {
+                                wishlistItems.add(product.id);
+                             });
+                              print(wishlistItems);
+                            },
                             icon: Icon(
-                              Icons.favorite_border,
+                              wishlistItems.contains(product.id)
+                                  ? Icons.favorite
+                                  : Icons.favorite_border,
                               color: Color(0xffDD8560),
                             ),
                           ),
