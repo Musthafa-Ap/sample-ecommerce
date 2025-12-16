1adf0d2a8bfa0e919328eb70a778e1a6ff079579
Musthafa <musthafamohammed398@gmail.com>
2025-12-16 20:27:21 +0530

    title changed to amazon

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Open Fashion',
+          title: 'Amazon',
           home: Scaffold(
             body: IndexedStack(
               index: currentIndex,
